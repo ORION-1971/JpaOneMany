15e0c076fd9db7615fa2a1e7337f147b821a46b9
ORION-1971 <kandme@rambler.ru>
2025-04-27 09:41:41 +0300

    27.04 One To One Uni

---
+++ src/main/java/com/jpa/OneToOneUni.java
@@ -0,0 +1,58 @@
+package com.jpa;
+
+import com.jpa.entity.Passport;
+import com.jpa.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.Scanner;
+
+public class OneToOneUni {
+
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введи имя студента");
+        String name = sc.nextLine();
+        System.out.println("Введи фамилию студента");
+        String surname = sc.nextLine();
+        System.out.println("Средняя оценка (5,5)");  // Вводить через запятую
+        double avgGrade = sc.nextDouble();
+
+        System.out.println("Введи email");
+        String email = scan.nextLine();
+        System.out.println("Цвет глаз");
+        String eyeColor = scan.nextLine();
+        System.out.println("Введи рост");
+        Integer height = scan.nextInt();
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-course");
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
+
+        try {
+            transaction.begin();                             //запуск транзакции
+            Student student = new Student(name, surname, avgGrade);
+            Passport passport = new Passport(email, height, eyeColor);
+                                                            /// @Очередность обязательна
+            student.setPassport(passport);                  // 1 - передача паспорта студенту
+            manager.persist(passport);                      // 2 - добавление паспорта в базу
+            manager.persist(student);                       // 3 - добавление студента в базу
+
+            transaction.commit();                           // совершить транзакцию
+
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();                     // откат транзакции
+            }
+            e.printStackTrace();
+        }
+        finally {
+            manager.close();
+            factory.close();
+        }
+    }
+}
---
+++ src/main/java/com/jpa/entity/Student.java
@@ -0,0 +1,85 @@
+package com.jpa.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students", schema = "test_db")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "avg_grade")
+    private Double avgGrade;
+
+    @OneToOne
+    @JoinColumn(name = "passport_id")
+    private Passport passport;
+
+
+    public Student(){}
+
+    public Student(String name, String surname, Double avgGrade) {
+        this.name = name;
+        this.surname = surname;
+        this.avgGrade = avgGrade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(Double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
